b0832c0bcdf55df8bc7d40391e4065c1edcd8f7e
eehcoo <ekojanuardipanca05@gmail.com>
2022-06-25 01:10:39 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/tubes/KatalogFotoUtil.java
@@ -0,0 +1,32 @@
+package com.example.tubes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KatalogFotoUtil {
+    private static int[] ridArray = {
+            R.drawable.spongebob,
+            R.drawable.patrick3,
+    };
+
+    private  static String[] filenameArray ={
+            "satu",
+            "dua",
+    };
+
+    private static List<KatalogFoto> katalogFotoList;
+
+    public static void init(){
+        katalogFotoList = new ArrayList<>();
+        int nArray = ridArray.length;
+        for(int i=0;i<nArray;i++){
+            katalogFotoList.add(new KatalogFoto(ridArray[i], filenameArray[i]));
+        }
+    }
+    public static List<KatalogFoto> getKatalogFotoList(){
+        return katalogFotoList;
+    }
+    public static KatalogFoto getKatalogFotoAt(int i){
+        return katalogFotoList.get(i);
+    }
+}
---
+++ app/src/main/java/com/example/tubes/KeranjangBelanjaListener.java
@@ -0,0 +1,5 @@
+package com.example.tubes;
+
+public interface KeranjangBelanjaListener {
+    void orderChanged();
+}
